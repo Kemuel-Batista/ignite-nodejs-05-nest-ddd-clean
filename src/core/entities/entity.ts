3007277dd6dec806b5d95e601d3ed39551dc1cb8
Kemuel Batista <kemuellima20@gmail.com>
2023-11-16 15:38:34 -0300

    fix: error on core/entity

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -10,7 +10,7 @@ export abstract class Entity<Props> {
 
   protected constructor(props: Props, id?: UniqueEntityID) {
     this.props = props
-    this._id = id ?? new UniqueEntityID(id)
+    this._id = id ?? new UniqueEntityID()
   }
 
   public equals(entity: Entity<unknown>) {
