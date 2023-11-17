07150c491d836552af91f196c39d149240779482
Kemuel Batista <kemuellima20@gmail.com>
2023-11-17 16:02:07 -0300

    feat: handling errors in controllers

--- src/infra/http/controllers/instructor/create-account-instructor.controller.ts
+++ src/infra/http/controllers/instructor/create-account-instructor.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
 import { z } from 'zod'
 import { ZodValidationPipe } from '../../pipes/zod-validation-pipe'
 import { RegisterInstructorUseCase } from '@/domain/forum/application/use-cases/instructor/register-instructor'
+import { InstructorAlreadyExistsError } from '@/domain/forum/application/use-cases/instructor/errors/instructor-already-exists-erros'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -28,7 +37,14 @@ export class CreateAccountInstructorController {
     })
 
     if (result.isError()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case InstructorAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
   }
 }
--- src/infra/http/controllers/student/create-account-student.controller.ts
+++ src/infra/http/controllers/student/create-account-student.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
 import { z } from 'zod'
 import { ZodValidationPipe } from '../../pipes/zod-validation-pipe'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/student/register-student'
+import { StudentAlreadyExistsError } from '@/domain/forum/application/use-cases/student/errors/student-already-exists-error'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -28,7 +37,14 @@ export class CreateAccountStudentController {
     })
 
     if (result.isError()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case StudentAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
     }
   }
 }
