831c0faa7195e50294772ab6c9803333aa4a98d3
Kemuel Batista <kemuellima20@gmail.com>
2023-11-17 15:09:23 -0300

    feat: register, authenticate student use case, tests and repository implementation

---
+++ src/domain/forum/application/use-cases/student/register-student.spec.ts
@@ -0,0 +1,59 @@
+import { RegisterStudentUseCase } from './register-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let sut: RegisterStudentUseCase
+
+describe('Register Student Use Case', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    sut = new RegisterStudentUseCase(inMemoryStudentsRepository, fakeHasher)
+  })
+
+  it('should be able to register a new student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(result.isSuccess()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0],
+    })
+  })
+
+  it('should hash student password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isSuccess()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const email = 'johndoe@example.com'
+
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(result.isError()).toBe(true)
+  })
+})
