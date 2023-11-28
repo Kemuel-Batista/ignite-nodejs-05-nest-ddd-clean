6dce685fd3a2a8d80d95e99b1e1510964e22314c
Kemuel Batista <kemuellima20@gmail.com>
2023-11-28 15:39:22 -0300

    feat: comment with author infra layer adjusts and implementation

--- src/infra/http/controllers/question/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/question/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { z } from 'zod'
 import { ZodValidationPipe } from '../../pipes/zod-validation-pipe'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/question/fetch-question-comments'
-import { CommentPresenter } from '../../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -39,10 +39,10 @@ export class FetchQuestionCommentsController {
       throw new BadRequestException()
     }
 
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
 
     return {
-      questionComments: questionComments.map(CommentPresenter.toHTTP),
+      comments: comments.map(CommentWithAuthorPresenter.toHTTP),
     }
   }
 }
